d2fd373fefe3c78d1ec6eb794bbaff70a39ac7c7
Lander Verhack <landerve@users.noreply.github.com>
2017-05-30 11:37:36 +0200

    added auth for spotify service

--- app/spotify.service.ts
+++ app/spotify.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, URLSearchParams } from '@angular/http';
+import { Http, URLSearchParams, RequestOptions, Headers } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -10,15 +10,28 @@ import 'rxjs/add/observable/throw';
 @Injectable()
 export class SpotifySearchService {
   private API_PATH = 'https://api.spotify.com/v1/search';
+  private LOGIN_PAGE = 'https://accounts.spotify.com/authorize?client_id=8aa495d9c8c24042b11fa8a561330200&redirect_uri=http:%2F%2Flocalhost:3002&response_type=token';
+  private token: string;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+    //check if auth token in url
+    if (window.location.hash.match(/#access_token/i)) {
+      this.token = window.location.hash.split('=')[1];
+    }
+    //if not redirect
+    else {
+      window.location.href = this.LOGIN_PAGE;
+    }
+  }
 
   search(query: string, type: string): Observable<string[]> {
-    var params = new URLSearchParams();
+    let params = new URLSearchParams();
     params.set('type', type);
     params.set('q', query);
 
-    return this.http.get(this.API_PATH, { search: params })
+    let headers = new Headers({ 'Authorization': `Bearer ${this.token}` });
+
+    return this.http.get(this.API_PATH, { search: params, headers: headers })
       .map(res => this.mapToNames(res.json(), type) || [])
       .catch(this.handleError);
   }
